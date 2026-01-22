93098bb0c19971b33004ad38836e808588877803
Jordan Grieve <grievejordan@yahoo.com>
2026-01-22 20:25:51 +0000

    implmenet candlestick chart

--- components/home/CoinOverview.tsx
+++ components/home/CoinOverview.tsx
@@ -1,49 +1,45 @@
 import { fetcher } from "@/lib/coingecko.actions";
 import Image from "next/image";
+import { CoinOverviewFallback } from "../home/fallback";
+import CandlestickChart from "../CandlestickChart";
 
 async function CoinOverview() {
-  let coin: CoinDetailsData | null = null;
-
   try {
-    coin = await fetcher<CoinDetailsData>("coins/bitcoin", {
-      dex_pair_format: "symbol",
-    });
-  } catch (error) {
-    console.error("Failed to fetch coin overview:", error);
-  }
+    const [coin, coinOHLCData] = await Promise.all([
+      await fetcher<CoinDetailsData>("coins/bitcoin", {
+        dex_pair_format: "symbol",
+      }),
+      await fetcher<OHLCData[]>("coins/bitcoin/ohlc", {
+        vs_currency: "usd",
+        days: 180, // 1, 7 , 14, 30, 90, 180, 365, max
+        precision: "full",
+      }),
+    ]);
 
-  if (!coin) {
     return (
       <div id="coin-overview">
-        <div className="header pt-2">
-          <div className="size-14 rounded-full bg-dark-400" />
-          <div className="info">
-            <p className="text-purple-100">Failed to load coin data</p>
-            <h1 className="text-muted-foreground">--</h1>
+        <CandlestickChart data={coinOHLCData} coinId="bitcoin">
+          <div className="header pt-2">
+            <Image
+              src={coin.image.large}
+              alt={`${coin.name} Logo`}
+              width={56}
+              height={56}
+            />
+            <div className="info">
+              <p>
+                {coin.name} / {coin.symbol.toUpperCase()}
+              </p>
+              <h1>${coin.market_data.current_price.usd.toFixed(2)}</h1>
+            </div>
           </div>
-        </div>
+        </CandlestickChart>
       </div>
     );
+  } catch (error) {
+    console.error("Failed to fetch coin overview:", error);
+    return <CoinOverviewFallback />;
   }
-
-  return (
-    <div id="coin-overview">
-      <div className="header pt-2">
-        <Image
-          src={coin.image.large}
-          alt={`${coin.name} Logo`}
-          width={56}
-          height={56}
-        />
-        <div className="info">
-          <p>
-            {coin.name} / {coin.symbol.toUpperCase()}
-          </p>
-          <h1>${coin.market_data.current_price.usd.toFixed(2)}</h1>
-        </div>
-      </div>
-    </div>
-  );
 }
 
 export default CoinOverview;
